9d520f301e40ecc678b0f532b612b5a07d514603
bureauvictor <82405991+bureauvictor@users.noreply.github.com>
2025-09-24 10:59:00 +0200

    Delete .github/workflows/hola_mundo.py

--- .github/workflows/hola_mundo.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def main():
-  nombre = os.getenv("USERNAME")
-  print(f'Hello World {nombre}, from Github. Hola Mundo wey{nombre}. I am learding Github Actions')
-
-if __name__ == "__main__":
-  main()
