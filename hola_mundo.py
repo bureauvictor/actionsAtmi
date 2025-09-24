2b1639a4ed5b4ae49284380fb0b185059edc3947
bureauvictor <82405991+bureauvictor@users.noreply.github.com>
2025-09-24 11:56:14 +0200

    Enhance hello world script with date display
    Added date retrieval and updated print statements.

--- hola_mundo.py
+++ hola_mundo.py
@@ -2,7 +2,9 @@ import os
 
 def main():
   nombre = os.getenv("USERNAME")
+  date = os.getdate('Date', '09-10-1990')
   print(f'Hello World {nombre}, from Github. Hola Mundo wey{nombre}. I am learding Github Actions')
+  print(f'Today is: {date}')
 
 if __name__ == "__main__":
   main()
