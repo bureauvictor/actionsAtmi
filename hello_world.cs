811037d95c58b0fa30adcd481a920f3b427140b7
bureauvictor <82405991+bureauvictor@users.noreply.github.com>
2025-09-24 13:01:06 +0200

    Change userName retrieval method in hello_world.cs

--- hello_world.cs
+++ hello_world.cs
@@ -2,6 +2,6 @@ using System;
 
 
 
-string userName = Environment.UserName;
+string userName = Environment.GetEnvironmentVariable("UserName");
 Console.WriteLine($"Hello World, it's {userName}!Today is:{DateTime.Now.ToString()}.");
 Console.WriteLine($"Hola Mundo, soy {userName}");
