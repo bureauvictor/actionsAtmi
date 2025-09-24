c513333138c329b7581392ff424093ea89d90d22
bureauvictor <82405991+bureauvictor@users.noreply.github.com>
2025-09-24 11:57:27 +0200

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -2,7 +2,7 @@ import os
 
 def main():
   nombre = os.getenv("USERNAME")
-  date = os.getdate('Date', '09-10-1990')
+  date = os.getenv('Date', '09-10-1990')
   print(f'Hello World {nombre}, from Github. Hola Mundo wey{nombre}. I am learding Github Actions')
   print(f'Today is: {date}')
 
