0d2fe964549b30ef9253960436983548d13fc4fe
bureauvictor <82405991+bureauvictor@users.noreply.github.com>
2025-09-24 12:55:08 +0200

    Refactor Program class to remove Main method

--- hello_world.cs
+++ hello_world.cs
@@ -1,10 +1,7 @@
 using System;
 
-class Program{
-    static void Main(string[] args){
 
-        string userName = Environment.UserName;
-        Console.WriteLine($"Hello World, it's {userName}!Today is:{DateTime.Now.ToString()}.");
-        Console.WriteLine($"Hola Mundo, soy {userName}");
-    }
-}
+
+string userName = Environment.UserName;
+Console.WriteLine($"Hello World, it's {userName}!Today is:{DateTime.Now.ToString()}.");
+Console.WriteLine($"Hola Mundo, soy {userName}");
